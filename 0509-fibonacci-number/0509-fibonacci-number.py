c505deb5abc326537e37e99e47f7cdc273ea458c
Shashank C  A <shashank1602ca@gmail.com>
2026-08-22 13:14:59 +0530

    Time: 668 ms (13.33%), Space: 12.4 MB (54.64%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def fib(self, n):
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+
+        return self.fib(n-1) + self.fib(n-2)    
+
+obj = Solution()
+answer = obj.fib(5)
+print(answer)
+        
