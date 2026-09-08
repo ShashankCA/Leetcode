240514ac9c4b0edea6857319abae4c1e4bfe8e96
Shashank C  A <shashank1602ca@gmail.com>
2026-09-08 18:30:48 +0530

    Time: 70 ms (68.07%), Space: 21.3 MB (44.44%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,16 +1,15 @@
 class Solution(object):
     def maxSubArray(self, nums):
         total = 0
-        maxi = float("-infinity")
-        
-        for num in nums:
-            total += num
-            maxi = max(total, maxi)
-            
+        maxi = float("-inf")
+
+        for i in range(len(nums)):
+            total = total + nums[i]
+            maxi = max(total,maxi)
             if total < 0:
                 total = 0
-                
         return maxi
+       
 
 
 
