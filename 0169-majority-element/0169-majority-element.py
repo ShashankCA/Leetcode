bd87e5e15eae4b3c95d7ed7c57d3ed27e8589291
Shashank C  A <shashank1602ca@gmail.com>
2026-09-16 12:17:17 +0530

    Time: 17 ms (17.17%), Space: 13.6 MB (40.64%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,16 @@
+class Solution(object):
+
+    def majorityElement(self, nums):
+
+        count = {}
+
+        for num in nums:
+            if num in count:
+                count[num] += 1
+            else:
+                count[num] = 1
+
+        for num in count:
+            if count[num] > len(nums) // 2:
+                return num
+        
